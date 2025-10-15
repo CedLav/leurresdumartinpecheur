280edcfacc23811d1064922d572251c402deeeac
CedLav <abyssalsnout@gmail.com>
2025-10-15 11:17:43 -0400

    Add files via upload

---
+++ productData.js
@@ -0,0 +1,422 @@
+const productData = [
+    {
+        id: 'a1',
+        title: 'Harnais #1',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #1'
+    },
+    {
+        id: 'a2',
+        title: 'Harnais #2',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #2'
+    },
+    {
+        id: 'a3',
+        title: 'Harnais #3',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #3'
+    },
+    {
+        id: 'a4',
+        title: 'Harnais #4',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #4'
+    },
+        {
+        id: 'a5',
+        title: 'Harnais #5',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #5'
+    },
+    {
+        id: 'a6',
+        title: 'Harnais #6',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #6'
+    },
+    {
+        id: 'a7',
+        title: 'Harnais #7',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #7'
+    },
+    {
+        id: 'a8',
+        title: 'Harnais #8',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #8'
+    },
+        {
+        id: 'a9',
+        title: 'Harnais #9',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #9'
+    },
+    {
+        id: 'a10',
+        title: 'Harnais #10',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #10'
+    },
+    {
+        id: 'a11',
+        title: 'Harnais #11',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #11'
+    },
+    {
+        id: 'a12',
+        title: 'Harnais #12',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #12'
+    },
+        {
+        id: 'a13',
+        title: 'Harnais #13',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #13'
+    },
+    {
+        id: 'a14',
+        title: 'Harnais #14',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #14'
+    },
+    {
+        id: 'a15',
+        title: 'Harnais #15',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #15'
+    },
+    {
+        id: 'a16',
+        title: 'Harnais #16',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #16'
+    },
+    {
+        id: 'a17',
+        title: 'Harnais #17',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #17'
+    },
+    {
+        id: 'a18',
+        title: 'Harnais #18',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #18'
+    },
+    {
+        id: 'a19',
+        title: 'Harnais #19',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #19'
+    },
+    {
+        id: 'a20',
+        title: 'Harnais #20',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du harnais #20'
+    },
+    {
+        id: 'b1',
+        title: 'Poisson-Nageur #1',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/1.jpg',
+        description: "Corps de Crankbait arrondi, bavette petite et peu inclinée. Couleur foncée avec des rayures. Idéal pour la prospection rapide en eau très peu profonde (moins de 1 mètre)."
+    },
+    {
+        id: 'b2',
+        title: 'Poisson-Nageur #2',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/2.jpg',
+        description: "Corps ventru, type Crankbait. Bavette moyenne. Couleur mauve irisée et jaune avec des marques de tigre. Génère une forte oscillation (wobble) et est excellent dans les eaux légèrement turbides."
+    },
+    {
+        id: 'b3',
+        title: 'Poisson-Nageur #3',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/3.jpg',
+        description: "Forme allongée de Minnow. Petite bavette très inclinée. Couleur irisée mauve et jaune. Action de nage serrée et discrète, ciblant les petits poissons prédateurs."
+    },
+    {
+        id: 'b4',
+        title: 'Poisson-Nageur #4',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/4.jpg',
+        description: "Corps très trapu. Grande bavette longue et large. Couleur vert, brun et rouge. Permet d'atteindre rapidement les fosses, idéal pour la pêche du doré en profondeur."
+    },
+    {
+        id: 'b5',
+        title: 'Poisson-Nageur #5',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/5.jpg',
+        description: "Corps ventru. Petite bavette très inclinée. Couleur vert olive/jaune avec des barres noires. Efficace pour rebondir sur les structures sans s'accrocher."
+    },
+    {
+        id: 'b6',
+        title: 'Poisson-Nageur #6',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/6.jpg',
+        description: "Corps de Crankbait ventru. Bavette large et longue. Couleur bleu électrique. Capacité de plongée importante combinée à une vibration puissante."
+    },
+    {
+        id: 'b7',
+        title: 'Poisson-Nageur #7',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/7.jpg',
+        description: "Très petit leurre, compact, sans bavette, avec anneau d'attache sur le dos. Couleur bleu foncé. Pour une pêche verticale rapide ou une prospection avec vibration intense."
+    },
+    {
+        id: 'b8',
+        title: 'Poisson-Nageur #8',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/8.jpg',
+        description: "Corps long et mince (Minnow). Bavette longue et étroite. Couleur orange avec rayures. Excellent pour cibler les poissons suspendus ou le doré en colonne d'eau."
+    },
+    {
+        id: 'b9',
+        title: 'Poisson-Nageur #9',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/9.jpg',
+        description: "Corps de Minnow plat et élancé. Bavette courte et inclinée. Couleur rouge foncé. Son profil plat augmente le \"flash\" latéral lors des animations."
+    },
+    {
+        id: 'b10',
+        title: 'Poisson-Nageur #10',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/10.jpg',
+        description: "Leurre très long et fin (Magnum Minnow). Bavette moyenne allongée. Couleur bleu foncé irisé. Conçu pour la longue distance et l'attrait des gros prédateurs."
+    },
+    {
+        id: 'b11',
+        title: 'Poisson-Nageur #11',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/11.jpg',
+        description: "Corps élancé, Minnow classique. Petite bavette, peu plongeante. Couleur jaune moutarde et noir. Nage près de la surface avec une action serrée (wiggling) pour un look réaliste."
+    },
+    {
+        id: 'b12',
+        title: 'Poisson-Nageur #12',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/12.jpg',
+        description: "Corps élancé et mince. Bavette courte. Couleur mauve/rose vif avec motifs noirs.  Le coloris agressif et unique est fait pour provoquer une attaque réflexe."
+    },
+    {
+        id: 'b13',
+        title: 'Poisson-Nageur #13',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/13.jpg',
+        description: "Corps très mince et droit, sans bavette. Couleur bleu très pâle avec des points pastel. Requiert la technique de Walk-the-Dog (zigzag) pour être efficace."
+    },
+    {
+        id: 'b14',
+        title: 'Poisson-Nageur #14',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/14.jpg',
+        description: "Forme de Minnow compacte, plus courte et dodue. Petite bavette. Couleur bleu profond. Sa petite taille permet une nage rapide et serrée, idéale pour la vitesse."
+    },
+    {
+        id: 'b15',
+        title: 'Poisson-Nageur #15',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/15.jpg',
+        description: "Forme de Minnow classique. Bavette longue et pointue, inclinée modérément. Couleur pourpre irisée et gris. Plonge à une profondeur moyenne (1,5-2 m), parfait pour les zones de transition."
+    },
+    {
+        id: 'b16',
+        title: 'Poisson-Nageur #16',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/16.jpg',
+        description: "Corps compact et trapu. Bavette petite et très inclinée. Couleur vert pomme et jaune fluorescent. La petite bavette et les couleurs vives sont parfaites pour les eaux sales et les lancers-ramenés rapides."
+    },
+    {
+        id: 'b17',
+        title: 'Poisson-Nageur #17',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/17.jpg',
+        description: "Corps compact en forme de goutte. Bavette courte et très inclinée. Couleur or métallique. L'aspect métallique offre un éclat maximal sous l'eau, idéal par temps ensoleillé."
+    },
+    {
+        id: 'b18',
+        title: 'Poisson-Nageur #18',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/18.jpg',
+        description: "Corps trapu et haut. Bavette petite et très inclinée. Couleur olive/jaune avec des rayures verticales. Ce leurre imite un poisson fourrage de manière réaliste ; il a une action de nage très vive et nerveuse."
+    },
+    {
+        id: 'b19',
+        title: 'Poisson-Nageur #19',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/19.jpg',
+        description: "Corps compact et haut. Bavette courte et inclinée. Couleur rouge vif et gris. La forme sans bavette et la couleur contrastée suggèrent un leurre bruyant et vibrant."
+    },
+    {
+        id: 'b20',
+        title: 'Poisson-Nageur #20',
+        price: '4.99$',
+        imageSrc: './assets/images/boutique/b/20.jpg',
+        description: "Leurres articulé en deux segments distincts. Il présente un corps ventru et un gros œillet. Couleur verte. Mouvement de nage en \"S\" très fluide et réaliste, ciblant les gros brochets ou achigans."
+    },    
+    {
+        id: 'c1',
+        title: 'Leurre souple #1',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #1'
+    },
+    {
+        id: 'c2',
+        title: 'Leurre souple #2',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #2'
+    },
+    {
+        id: 'c3',
+        title: 'Leurre souple #3',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #3'
+    },
+    {
+        id: 'c4',
+        title: 'Leurre souple #4',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #4'
+    },
+    {
+        id: 'c5',
+        title: 'Leurre souple #5',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #5'
+    },
+    {
+        id: 'c6',
+        title: 'Leurre souple #6',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #6'
+    },
+    {
+        id: 'c7',
+        title: 'Leurre souple #7',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #7'
+    },
+    {
+        id: 'c8',
+        title: 'Leurre souple #8',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #8'
+    },
+    {
+        id: 'c9',
+        title: 'Leurre souple #9',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #9'
+    },
+    {
+        id: 'c10',
+        title: 'Leurre souple #10',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #10'
+    },
+    {
+        id: 'c11',
+        title: 'Leurre souple #11',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #11'
+    },
+    {
+        id: 'c12',
+        title: 'Leurre souple #12',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #12'
+    },
+    {
+        id: 'c13',
+        title: 'Leurre souple #13',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #13'
+    },
+    {
+        id: 'c14',
+        title: 'Leurre souple #14',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #14'
+    },
+    {
+        id: 'c15',
+        title: 'Leurre souple #15',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #15'
+    },
+    {
+        id: 'c16',
+        title: 'Leurre souple #16',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #16'
+    },
+    {
+        id: 'c17',
+        title: 'Leurre souple #17',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #17'
+    },
+    {
+        id: 'c18',
+        title: 'Leurre souple #18',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #18'
+    },
+    {
+        id: 'c19',
+        title: 'Leurre souple #19',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #19'
+    },
+    {
+        id: 'c20',
+        title: 'Leurre souple #20',
+        price: '4.99$',
+        imageSrc: './assets/images/logo.jpg',
+        description: 'Description du leurre souple #20'
+    },
+]
